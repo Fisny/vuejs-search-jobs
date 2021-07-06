5cfe44ef2d31459e62445e85aacd16e440c7687c
Fisny <bardi.fisniku@gmail.com>
2021-07-06 16:17:26 +0200

    5

--- js/script.js
+++ js/script.js
@@ -6,14 +6,54 @@ new Vue ({
         jobs: [
             {
               id: 1,
-              company: 'Perferendis',
+              company: 'Microsoft',
               position: 'Developer',
-              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
               created_at: '5/22/2021',
               logo: 'logo.jpg',
               city: 'Roma',
               contract: 'Full Time'
-            }
+            },
+            {
+              id: 2,
+              company: 'Red Hat',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '2/22/2021',
+              logo: 'logo.jpg',
+              city: 'Genova',
+              contract: 'Full Time'
+            },
+            {
+              id: 3,
+              company: 'General Electric',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'logo.jpg',
+              city: 'Roma',
+              contract: 'Full Time'
+            },
+            {
+              id: 4,
+              company: 'Leonardo',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'logo.jpg',
+              city: 'Roma',
+              contract: 'Full Time'
+            },
+            {
+              id: 5,
+              company: 'ericsson',
+              position: 'Developer',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'https://media-exp1.licdn.com/dms/image/C4D0BAQFp4NCYZynPZQ/company-logo_200_200/0/1519855897171?e=2159024400&v=beta&t=0kAhKO-hvzzlIjX9yGKbb_NKU59UxBc4VBBCcz8qOoI',
+              city: 'Roma',
+              contract: 'Full Time'
+            },
         ],
         starred: [1, 2, 3],
         applied: [4, 5]        
