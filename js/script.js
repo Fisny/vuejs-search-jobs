8d528cf0df518d5400079db22a81780a6ee1de1f
Fisny <bardi.fisniku@gmail.com>
2021-07-06 16:39:02 +0200

    img

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ new Vue ({
               position: 'Developer',
               description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
               created_at: '5/22/2021',
-              logo: 'logo.jpg',
+              logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/480px-Microsoft_logo.svg.png',
               city: 'Roma',
               contract: 'Full Time'
             },
@@ -20,7 +20,7 @@ new Vue ({
               position: 'Developer',
               description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
               created_at: '2/22/2021',
-              logo: 'logo.jpg',
+              logo: 'https://media-exp1.licdn.com/dms/image/C4E0BAQEto-TydTTIfQ/company-logo_200_200/0/1556712048684?e=2159024400&v=beta&t=gKkSK5xViTuWTIMZSLpnpiNXMQ2Z1IdmPLYFanCsEag',
               city: 'Genova',
               contract: 'Full Time'
             },
@@ -30,7 +30,7 @@ new Vue ({
               position: 'Developer',
               description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
               created_at: '5/22/2021',
-              logo: 'logo.jpg',
+              logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/General_Electric_logo.svg/1024px-General_Electric_logo.svg.png',
               city: 'Roma',
               contract: 'Full Time'
             },
@@ -40,7 +40,7 @@ new Vue ({
               position: 'Developer',
               description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
               created_at: '5/22/2021',
-              logo: 'logo.jpg',
+              logo: 'https://www.repstatic.it/content/nazionale/img/2016/08/24/103147840-e7eb392f-a847-4e3e-a4fd-504d167703f8.jpg',
               city: 'Roma',
               contract: 'Full Time'
             },
